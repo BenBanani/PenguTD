3595b38a07c1d4ebbe9d178670cf450743f5c991
Lordkerimus <phujjddc@gmail.com>
2026-04-24 18:07:14 +0200

    Add files via upload

---
+++ assets/core/src/main/java/info/pengutd/game/GameScreen.java
@@ -0,0 +1,139 @@
+package info.pengutd.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import info.pengutd.game.enemy.Enemy;
+import info.pengutd.game.enemy.NormalEnemy;
+
+public class GameScreen implements Screen, InputProcessor {
+
+    private SpriteBatch batch;
+    private Viewport viewport;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer mapRenderer;
+    private Enemy testEnemey;
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(this);
+        batch = new SpriteBatch();
+        map = new TmxMapLoader().load("map/map1.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
+
+        int mapWidth = map.getProperties().get("width", Integer.class);
+        int mapHeight = map.getProperties().get("height", Integer.class);
+        int tileWidth = map.getProperties().get("tilewidth", Integer.class);
+        int tileHeight = map.getProperties().get("tileheight", Integer.class);
+
+        viewport = new FitViewport(
+            mapWidth * tileWidth,
+            mapHeight * tileHeight
+        );
+        testEnemey = new NormalEnemy(4, map);
+    }
+
+    public TiledMap getMap() {
+        return map;
+    }
+
+    @Override
+    public void render(float delta) {
+        testEnemey.move(delta);
+
+        ScreenUtils.clear(Color.BLACK);
+
+        viewport.apply();
+
+        mapRenderer.setView((OrthographicCamera) viewport.getCamera());
+        mapRenderer.render();
+
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+        batch.begin();
+        testEnemey.draw(batch);
+        batch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        mapRenderer.dispose();
+    }
+
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        testEnemey.pop(1);
+        return true;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+}
---
+++ assets/core/src/main/java/info/pengutd/game/enemy/Enemy.java
@@ -0,0 +1,42 @@
+package info.pengutd.game.enemy;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Disposable;
+
+public abstract class Enemy implements Disposable {
+    public static final float HEIGHT = 10f;
+    public static final float WIDTH = 10f;
+    public abstract Texture getTexture();
+
+    public abstract int getHealth();
+
+    public abstract float getSpeed();
+
+    public abstract Array<Vector2> getPath();
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(getTexture(), getX() - getWidth() / 2, getY() - getHeight() / 2, -getWidth(), getHeight());
+    }
+
+    public abstract float getHeight();
+
+    public abstract float getWidth();
+
+    public abstract float getX();
+
+    public abstract float getY();
+
+    public abstract void move(float delta);
+
+    @Override
+    public void dispose() {
+        getTexture().dispose();
+    }
+
+    public abstract void pop(int damage);
+
+    public abstract void die();
+}
