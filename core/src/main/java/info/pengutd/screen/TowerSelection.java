5d1fec380b243f04fa8c4044bac81ebf00a6b84e
Martin <mpmail1@gmx.de>
2026-04-29 17:14:44 +0200

    das große blaue Kästchen oben in der Tower Selection
    Took 34 minutes

--- core/src/main/java/info/pengutd/screen/TowerSelection.java
+++ core/src/main/java/info/pengutd/screen/TowerSelection.java
@@ -4,59 +4,62 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class TowerSelection implements Disposable {
     private final Stage uiStage;
     private final Texture backgroundTexture;
+    private final Texture topButtonTexture;
 
     public TowerSelection(Viewport viewport) {
         uiStage = new Stage(viewport);
         Gdx.input.setInputProcessor(new InputMultiplexer(Gdx.input.getInputProcessor(), uiStage));
 
         backgroundTexture = new Texture(Gdx.files.internal("game/tower_selection_screen/background.png"));
-
+        topButtonTexture = new Texture(Gdx.files.internal("game/tower_selection_screen/button_blue.png"));
 
         // table auf ganzem Screen
         Table root = new Table();
         root.setFillParent(true);
 
         // sidebar table
-        Table table = new Table();
-        table.setBackground(
-            new TextureRegionDrawable(new TextureRegion(backgroundTexture)).tint(new Color(1, 1, 1, 0.6f))  // 30% transparent
+        Table sidebar = new Table();
+        sidebar.top();
+        sidebar.setBackground(
+            new TextureRegionDrawable(new TextureRegion(backgroundTexture)).tint(new Color(1, 1, 1, 0.6f))  // 40% transparent
         );
+        sidebar.defaults().growX().pad(10);
 
-        table.defaults().pad(10);
-        table.padTop(20);
-
-        // linker Bereich nimmt Rest ein
+        // linker Bereich (spiel)
         root.add().expandX().growY();
         // rechte Sidebar
-        root.add(table).width(200).growY();
+        root.add(sidebar).width(200).growY();
 
-        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
-
-        TextButton cannonButton = new TextButton("Cannon", skin);
-        TextButton iceButton = new TextButton("Ice", skin);
-        TextButton sniperButton = new TextButton("Sniper", skin);
-
-        table.add(cannonButton).width(180).height(60).padBottom(10);
-        table.row();
-
-        table.add(iceButton).width(180).height(60).padBottom(10);
-        table.row();
-
-        table.add(sniperButton).width(180).height(60);
-
+        Stack stack = topElement();
+        sidebar.add(stack).width(180).height(180).padBottom(10);
+        sidebar.row();
         uiStage.addActor(root);
+    }
+
+    /// das blaue rechteck oben, in dem Geld und Hp angezeigt werden
+    private Stack topElement() {
+        Stack stack = new Stack();
+        Image topBackground = new Image(topButtonTexture);
+        topBackground.getColor().a = 0.9f;
+        topBackground.setScaling(Scaling.stretch);
+        Table topContent = new Table();
+        topContent.add(new Label("Money", new Label.LabelStyle(new BitmapFont(), Color.WHITE))).pad(10).row();
+        topContent.add(new Label("HP", new Label.LabelStyle(new BitmapFont(), Color.WHITE))).pad(10);
+        stack.add(topBackground);
+        stack.add(topContent);
+        return stack;
     }
 
     public void render(float delta) {
