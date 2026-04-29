e286192975bf0b4f7edf81bf6d37ec3569815ee4
Martin <mpmail1@gmx.de>
2026-04-29 23:53:57 +0200

    SettingsScreen bisschen schöner
    Took 23 minutes

--- core/src/main/java/info/pengutd/screen/SettingsScreen.java
+++ core/src/main/java/info/pengutd/screen/SettingsScreen.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -22,9 +23,10 @@ public class SettingsScreen implements Screen {
     private Stage stage;
     private Texture backgroundTexture;
     private TextButton fullscreenButton;
-    private TextButton backButton;
+    private ImageButton backButton;
     private Texture titleTexture;
     private Texture buttonTexture;
+    private Texture backButtonTexture;
 
     public SettingsScreen(Screen screen) {
         oldScreen = screen;
@@ -66,7 +68,7 @@ public class SettingsScreen implements Screen {
     private void buildUI() {
         Table root = new Table();
         root.setFillParent(true);
-        root.debug().center().top();
+        root.debug().top();
 
         backgroundTexture = new Texture(Gdx.files.internal("background.png"));
         Image background = new Image(backgroundTexture);
@@ -77,21 +79,25 @@ public class SettingsScreen implements Screen {
 
         titleTexture = new Texture(Gdx.files.internal("settings_screen/title.png"));
         Image title = new Image(titleTexture);
-        root.add(title).width(300).height(108).colspan(2).padTop(40).padBottom(25).row();
+        root.add(title).width(300).height(108).colspan(2).padTop(25).padBottom(25).row();
 
         Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
 
-        root.add(createSlider(skin, "Sound Volume")).width(300).height(50).pad(10).row();
+        root.add(createSlider(skin, "Sound Volume")).width(300).height(75).pad(5).row();
 
-        root.add(createSlider(skin, "Music Volume")).width(300).height(50).pad(10).row();
+        root.add(createSlider(skin, "Music Volume")).width(300).height(75).pad(5).row();
 
-        fullscreenButton = new TextButton("Fullscreen: " + (Settings.get().getFullScreen() ? "On" : "Off"), skin);
-        root.add(fullscreenButton).row();
+        root.add(createFullscreenButton(skin)).width(300).height(75).pad(5).row();
 
-        backButton = new TextButton("Back", skin);
-        root.add(backButton).row();
+        Table topLeft = new Table();
+        topLeft.setFillParent(true);
+        topLeft.top().left();
+        backButtonTexture = new Texture(Gdx.files.internal("settings_screen/back_button.png"));
+        backButton = new ImageButton(new TextureRegionDrawable(backButtonTexture));
+        topLeft.add(backButton).size(50, 50).pad(25).row();
 
         stage.addActor(background);
+        stage.addActor(topLeft);
         stage.addActor(root);
     }
 
@@ -105,6 +111,19 @@ public class SettingsScreen implements Screen {
         Slider musicSlider = new Slider(0, 1, 0.01f, false, skin);
         musicSlider.setValue(Settings.get().getMusicVolume());
         content.add(musicSlider);
+        stack.add(content);
+
+        return stack;
+    }
+
+    private Stack createFullscreenButton(Skin skin) {
+        Stack stack = new Stack();
+        Image background = new Image(buttonTexture);
+        stack.add(background);
+        Table content = new Table();
+        content.setFillParent(true);
+        fullscreenButton = new TextButton("Fullscreen: " + (Settings.get().getFullScreen() ? "On" : "Off"), skin);
+        content.add(fullscreenButton).row();
         stack.add(content);
 
         return stack;
@@ -145,6 +164,7 @@ public class SettingsScreen implements Screen {
         backgroundTexture.dispose();
         titleTexture.dispose();
         buttonTexture.dispose();
+        backButtonTexture.dispose();
         // textureAtlas.dispose();
     }
 }
