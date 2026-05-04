6cfd4dd2e7385b5610e08a7c8a622e8d85d3f190
Martin <mpmail1@gmx.de>
2026-05-04 12:05:05 +0200

    Kommentare
    Took 3 minutes

--- core/src/main/java/info/pengutd/save/JsonSerializable.java
+++ core/src/main/java/info/pengutd/save/JsonSerializable.java
@@ -2,6 +2,10 @@ package info.pengutd.save;
 
 import com.badlogic.gdx.utils.JsonValue;
 
+/// Interface für Klassen, die als JSON serialisiert werden können um spielstände zu speichern
+/// die Json dateien sollten in assets/saves gespeichert werden
+/// jedes Json Objekt muss ein type Feld haben
+/// todo alex tests hierfür machen
 public interface JsonSerializable {
     JsonValue toJson();
     void fromJson(JsonValue json);
