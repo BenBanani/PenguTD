80b7a6c46015e156791fd74c1cf930f28762332d
Martin <mpmail1@gmx.de>
2026-05-20 11:08:38 +0200

    remove PenguTd-main
    Took 1 minute

--- core/src/test/java/info/pengutd/TexturePackerHelperTest.java
+++ core/src/test/java/info/pengutd/TexturePackerHelperTest.java
@@ -12,59 +12,56 @@ class TexturePackerHelperTest {
 
     @Test
     void main_callsTexturePackerProcessForEveryAtlas() {
-        MockedStatic<TexturePacker> mocked = mockStatic(TexturePacker.class);
-        try {
+        try (MockedStatic<TexturePacker> mocked = mockStatic(TexturePacker.class)) {
             TexturePackerHelper.main(new String[]{});
 
             mocked.verify(() -> TexturePacker.process(
-                    "assets/start_screen",
-                    "assets/atlas",
-                    "start_screen_ui"
+                "assets/start_screen",
+                "assets/atlas",
+                "start_screen_ui"
             ));
 
             mocked.verify(() -> TexturePacker.process(
-                    "assets/settings_screen",
-                    "assets/atlas",
-                    "settings_screen_ui"
+                "assets/settings_screen",
+                "assets/atlas",
+                "settings_screen_ui"
             ));
 
             mocked.verify(() -> TexturePacker.process(
-                    "assets//game/tower_selection_screen",
-                    "assets/atlas",
-                    "tower_selection_ui"
+                "assets//game/tower_selection_screen",
+                "assets/atlas",
+                "tower_selection_ui"
             ));
 
             mocked.verify(() -> TexturePacker.process(
-                    "assets//game/pause_screen",
-                    "assets/atlas",
-                    "pause_screen_ui"
+                "assets//game/pause_screen",
+                "assets/atlas",
+                "pause_screen_ui"
             ));
 
             mocked.verify(() -> TexturePacker.process(
-                    "assets//game/enemy",
-                    "assets/atlas",
-                    "enemies"
+                "assets//game/enemy",
+                "assets/atlas",
+                "enemies"
             ));
 
             mocked.verify(() -> TexturePacker.process(
-                    "assets//game/tower/projectile",
-                    "assets/atlas",
-                    "projectiles"
+                "assets//game/tower/projectile",
+                "assets/atlas",
+                "projectiles"
             ));
 
             mocked.verify(() -> TexturePacker.process(
-                    "assets//game/tower/",
-                    "assets/atlas",
-                    "towers"
+                "assets//game/tower/",
+                "assets/atlas",
+                "towers"
             ));
 
             // Ensure no unexpected extra calls were made
             mocked.verify(
-                    () -> TexturePacker.process(anyString(), anyString(), anyString()),
-                    times(7)
+                () -> TexturePacker.process(anyString(), anyString(), anyString()),
+                times(7)
             );
-        } finally {
-            mocked.close();
         }
     }
 }
